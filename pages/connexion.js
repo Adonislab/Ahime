a9f7cbb0541efd8b8b77ad6a2cf6bd66d4b64582
Adonis <nobimetanguy19@gmail.com>
2024-05-23 21:24:15 +0100

    Suppression de l'option création de compte

--- pages/connexion.js
+++ pages/connexion.js
@@ -46,11 +46,7 @@ export default function Home() {
                 </div>
                 <button type="submit" className="w-full text-white bg-indigo-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Connexion</button>
               </form>
-              <div className="mt-4">
-                <button onClick={handleSignUpRedirect} className="text-indigo-600 hover:text-indigo-700 dark:text-white dark:hover:text-gray-300 font-medium">
-                  J'ai pas encore de compte
-                </button>
-              </div>
+              
             </div>
           </div>
         </div>
